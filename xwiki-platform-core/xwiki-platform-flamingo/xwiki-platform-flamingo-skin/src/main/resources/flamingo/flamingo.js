1cef2ef030c7821321498d2938889832ba40a99a
Guillaume Delhumeau <guillaume.delhumeau@xwiki.com>
2015-08-11 15:58:25 +0200

    XWIKI-12189: Simplify the top menu and add a drawer.
    * WIP

--- xwiki-platform-core/xwiki-platform-flamingo/xwiki-platform-flamingo-skin/src/main/resources/flamingo/flamingo.js
+++ xwiki-platform-core/xwiki-platform-flamingo/xwiki-platform-flamingo-skin/src/main/resources/flamingo/flamingo.js
@@ -1,12 +1,34 @@
 require(['jquery', 'bootstrap'], function($) {
-  // Bugfix: TODO: remove this when https://github.com/twbs/bootstrap/issues/16968 is fixed
-  $('.dropdown').on('shown.bs.dropdown', function () {
-    var menu = $(this).find('.dropdown-menu');
-    var menuLeft = menu.offset().left;
-    var menuWidth = menu.outerWidth();
-    var documentWidth = $(body).outerWidth();
-    if (menuLeft + menuWidth > documentWidth) {
-      menu.offset({'left': documentWidth - menuWidth});
-    }
-  })
+  $(document).ready(function() {
+  
+    // Bugfix: TODO: remove this when https://github.com/twbs/bootstrap/issues/16968 is fixed
+    $('.dropdown').on('shown.bs.dropdown', function () {
+      var menu = $(this).find('.dropdown-menu');
+      var menuLeft = menu.offset().left;
+      var menuWidth = menu.outerWidth();
+      var documentWidth = $(body).outerWidth();
+      if (menuLeft + menuWidth > documentWidth) {
+        menu.offset({'left': documentWidth - menuWidth});
+      }
+    });
+    
+    // Show/hide the global search
+    var globalSearch = $('#globalsearch');
+    globalSearch.hide();
+    $('#globalsearch-trigger').click(function(event) {
+      var icon = $(this).find('span');
+      if (icon.hasClass('glyphicon-search')) {
+        icon.removeClass('glyphicon-search');
+        icon.addClass('glyphicon-remove');
+        $('#globalsearch').fadeIn();
+      } else {
+        icon.removeClass('glyphicon-remove');
+        icon.addClass('glyphicon-search');
+        $('#globalsearch').fadeOut();
+      }
+      
+      return false;
+    });
+  
+  });
 });
